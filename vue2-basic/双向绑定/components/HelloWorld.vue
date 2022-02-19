4048c4d9beb9376e2471c5da8857be8a94001a76
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-02-19 11:31:13 +0800

    双向绑定

---
+++ vue2-basic/双向绑定/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+		<div id="demo">
+			<h1>Hello，{{name.toUpperCase()}}，{{address}}</h1>
+		</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+			name:'atguigu',
+			address:'北京'
+    };
+  }
+}
+</script>
