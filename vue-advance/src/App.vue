5ff3376a037e4eb4aadb42edab0fa15a0d921cc5
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-02-20 21:23:04 +0800

    创建了项目

--- vue-advance/src/App.vue
+++ vue-advance/src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div>
-		<button>原生的按钮</button>
-		<input type="text">
-		<atguigu-row>
-			<el-button>默认按钮</el-button>
-			<el-button type="primary">主要按钮</el-button>
-			<el-button type="success">成功按钮</el-button>
-			<el-button type="info">信息按钮</el-button>
-			<el-button type="warning">警告按钮</el-button>
-			<el-button type="danger">危险按钮</el-button>
-		</atguigu-row>
-		<atguigu-date-picker
-      type="date"
-      placeholder="选择日期">
-    </atguigu-date-picker>
-		<atguigu-row>
-			<el-button icon="el-icon-search" circle></el-button>
-			<el-button type="primary" icon="el-icon-s-check" circle></el-button>
-			<el-button type="success" icon="el-icon-check" circle></el-button>
-			<el-button type="info" icon="el-icon-message" circle></el-button>
-			<el-button type="warning" icon="el-icon-star-off" circle></el-button>
-			<el-button type="danger" icon="el-icon-delete" circle></el-button>
-		</atguigu-row>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-	export default {
-		name:'App',
-	}
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
