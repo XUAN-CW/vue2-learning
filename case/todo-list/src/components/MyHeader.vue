bf5b94fc0a6e8287c891f19cf37413d9c1c915b6
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-02-22 10:44:43 +0800

    Update MyHeader.vue

--- case/todo-list/src/components/MyHeader.vue
+++ case/todo-list/src/components/MyHeader.vue
@@ -10,7 +10,6 @@
 </template>
 
 <script>
-import { nanoid } from "nanoid";
 export default {
   name: "MyHeader",
   //接收从App传递过来的addTodo
@@ -26,7 +25,11 @@ export default {
       //校验数据
       if (!this.title.trim()) return alert("输入不能为空");
       //将用户的输入包装成一个todo对象
-      const todoObj = { id: nanoid(), title: this.title, done: false };
+      const todoObj = {
+        id: Date.now() + Math.random(),
+        title: this.title,
+        done: false,
+      };
       //通知App组件去添加一个todo对象
       this.addTodo(todoObj);
       //清空输入
